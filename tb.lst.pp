ee4de0cb6fde6bf6ebe61a3c0f85f032ec6aa512
Lyren Brown <lyren.brown@gmail.com>
2014-04-08 22:26:45 -0500

    simplify

--- tb.lst.pp
+++ tb.lst.pp
@@ -1,4 +1,4 @@
->>> sub hx { sprintf "\$%x", $_[0] }
+>>> sub hx { sprintf "\$%X", $_[0] }
 >>> open $fh, "mem.bin.lz4ish" or die "ERROR: Cannot open mem.bin.lz4ish: $!\n";
 >>> binmode $fh;
 >>> read $fh, $bin, 999999;
@@ -20,35 +20,57 @@ i=$A000:
 o=$4800:
 while i< <<<hx(0xA000+$count)>>>:
 >>> #  ? hex$(i);".";hex$(o);".";:
-  :
-  ?0;:
-  l=peek(i):
-  mo=dpeek(i+1):
-  r=l div 16:
-  m=l & 15:
-  i=i+3:
-  if r=15:
-    r=peek(i):
-    i=i+1:
-  endif:
-  if m=15:
-    m=dpeek(i):
-    i=i+2:
-  endif:
->>> #  ? hex$(l);".";hex$(mo);".";hex$(r);".";hex$(m):
-  move i,o,r:
-  i=i+r:
-  o=o+r:
-  move o-mo,o,m+4:
-  o=o+m+4:
+    :
+    ?0;:
+    l=peek(i):
+    mo=dpeek(i+1):
+    r=l div 16:
+    m=l & 15:
+    i=i+3:
+    if r=15:
+        r=peek(i):
+        i=i+1:
+    endif:
+    if m=15:
+        m=dpeek(i):
+        i=i+2:
+    endif:
+>>> #    ? hex$(l);".";hex$(mo);".";hex$(r);".";hex$(m):
+    move i,o,r:
+    i=i+r:
+    o=o+r:
+    move o-mo,o,m+4:
+    o=o+m+4:
 wend:
 pause 0:
 move $4A03,$D400,15:
 :
 move $4A12,$D000,31:
->>>#do:
->>>#v=0:
->>>#poke $D01E,0:
+>>> $simple = 1;
+>>> if ($simple) {
+do:
+v=0:
+poke $D01E,0:
+s=$5000:
+x=100:
+sound 0,0,10,6:
+repeat:
+    v=v+1:
+    if v=16:v=0:s=s+32:endif:
+    j=peek($D300):
+    x=x+(j=$F7)-(j=$FB):
+    while peek($d40b)<110:wend:
+    :
+    poke $D405,v:
+    dpoke $49D1,s:
+    poke $D000,x:
+    move $49E3+v,$D200,1:
+    move $49F3+v,$D018,1:
+until peek($D004) or peek($D00C):
+sound 0,48,0,15:for i=0 to 999:next i:
+poke $D000,0:
+loop:
+>>> } else {
 s=$5000:
 x=100:
 b=255:
@@ -93,9 +115,8 @@ repeat:
 until peek($D004) or peek($D00C):
 :
 sound 0,48,0,15:for i=0 to 999:next i:
->>>#poke $D000,0:
->>>#loop:
 move $4AE0,$D000,31:
 poke $D40E,$40:
 graphics 0:
 run:
+>>> }
